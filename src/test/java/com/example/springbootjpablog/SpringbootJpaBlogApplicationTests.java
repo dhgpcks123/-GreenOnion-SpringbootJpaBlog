0d8e62e8a371737c806290f7ee99a55b2229af8e
hyechan <dhgpcks@gmail.com>
2022-05-01 15:39:01 +0900

    init

---
+++ src/test/java/com/example/springbootjpablog/SpringbootJpaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjpablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
