c629e333a368a588f65345cc70df66fa467eb21a
hyechan <dhgpcks@gmail.com>
2022-05-02 15:52:17 +0900

    Board 엔티티 주석 추가

--- src/main/java/com/example/springbootjpablog/entity/Board.java
+++ src/main/java/com/example/springbootjpablog/entity/Board.java
@@ -22,17 +22,18 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false, length = 150) // 1 )
+    @Column(nullable = false, length = 150) // 1 ) length 지정할 수 있어요
     private String title;
 
-    @Lob // 2 )
+    @Lob // 2 ) Large Object 큰 데이터 저장할 때 사용합니다
     @Column(nullable = false)
     private String content;
 
-    @CreationTimestamp // 3 )
+    @CreationTimestamp // 3 ) 데이터가 입력될 때 현재 시간을 채워 INSERT해줍니다
     private Timestamp createDate;
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE) // 4
+    @OneToMany(mappedBy = "board", cascade = CascadeType.REMOVE)
+    // 4 ) CascadeType.REMOVE, board의 데이터가 imageList에 들어있는 데이터도 db에서 삭제됩니다
     private List<Image> imageList = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
